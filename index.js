e0c3e721573f63617d8927112a25f4be112ae6f8
Tim Disney <tim.disney@gmail.com>
2016-03-20 11:10:36 -0700

    Add ability to specify the node version

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ module.exports = {
         tonic: {
             process: function(block) {
                 var readOnly = Boolean(block.kwargs.readOnly);
+                var nodeVersion = '';
+                if (block.kwargs.nodeVersion) {
+                  nodeVersion = 'data-node-version="' + block.kwargs.nodeVersion + '"';
+                }
                 var className = 'pg-tonic';
                 if (readOnly) className += ' readonly';
 
-                return '<pre class="' + className + '">' + block.body + '</pre>';
+                return '<pre class="' + className + '"' + nodeVersion + '>' + block.body + '</pre>';
             }
         }
     }
